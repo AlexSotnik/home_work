a0f26f7fa7e3afdbeba47d0bf2a543339ac8b6f1
Aleksandr_Sotnikov <Sotnik617@yandex.ru>
2024-06-18 18:22:07 +0800

    add def get_data

--- src/widget.py
+++ src/widget.py
@@ -8,5 +8,7 @@ def mask_account_card(card_number: str) -> str:
         type_card = f"{card_number[0:len(card_number)-16]}"
     return f"{type_card}  {number}"
 
-
+def get_data(date_time: str) -> str:
+    """Возвращает дату ДД.ММ.ГГ"""
+    return f"{date_time[8:10]}.{date_time[5:7]}.{date_time[0:4]}"
 
